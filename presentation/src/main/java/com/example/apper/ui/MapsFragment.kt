e4a0995388296b5f6393f3d3d63e289525a7c0e6
buithanhdat <buithanhdat15051997@gmail.com>
2024-06-07 15:21:16 +0700

    Impl:Add google map api

--- presentation/src/main/java/com/example/apper/ui/MapsFragment.kt
+++ presentation/src/main/java/com/example/apper/ui/MapsFragment.kt
@@ -1,17 +1,21 @@
 package com.example.apper.ui
 
+import androidx.fragment.app.Fragment
+
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.example.apper.R
 import com.example.apper.ui.base.BaseFragment
+
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : BaseFragment(R.layout.fragment_map), OnMapReadyCallback {
+class MapsFragment : BaseFragment(R.layout.fragment_maps) {
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -27,17 +31,10 @@ class MapFragment : BaseFragment(R.layout.fragment_map), OnMapReadyCallback {
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
-    private var mGoogleMap: GoogleMap? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val mapFragment =
-            childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment?
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mGoogleMap = googleMap
     }
 }
